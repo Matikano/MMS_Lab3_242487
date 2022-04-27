f3a00ad5572dca9fb10438e187a3220178b46be6
Matikano <56170622+Matikano@users.noreply.github.com>
2022-04-27 08:07:12 +0200

    Initial commit

---
+++ app/src/main/java/pl/edu/pwr/lab3/i242487/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.edu.pwr.lab3.i242487
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
