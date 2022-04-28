3546a2b6761b1c37ddcd60d7cfa6b289b58751d3
Matikano <56170622+Matikano@users.noreply.github.com>
2022-04-28 19:19:06 +0200

    c)
    but it doesnt work on my phone, maybe it will work on another ...

---
+++ app/src/main/java/pl/edu/pwr/lab3/i242487/view/GraphicOverlay.kt
@@ -0,0 +1,132 @@
+package pl.edu.pwr.lab3.i242487.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.util.AttributeSet
+import android.view.View
+import com.google.firebase.components.Preconditions
+
+public class GraphicOverlay(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+    init {
+        addOnLayoutChangeListener {
+                _, _, _, _, _, _, _, _, _ -> needUpdateTransformation = true
+        }
+    }
+
+    private val lock = Any()
+
+    private var graphics = mutableListOf<Graphic>()
+    private val transformationMatrix = Matrix()
+
+    private var imageWidth: Int = 0
+    private var imageHeight: Int = 0
+
+    private var scaleFactor = 1.0f
+
+    private var postScaleWidthOffset: Float = 0f
+    private var postScaleHeightOffset: Float = 0f
+
+    private var isImageFlipped: Boolean?= null
+    private var needUpdateTransformation = true
+
+    public abstract class Graphic(private var overlay: GraphicOverlay){
+
+        val context: Context = overlay.context.applicationContext
+
+        val isImageFlipped = overlay.isImageFlipped
+
+        val transformationMatrix = overlay.transformationMatrix
+
+        public abstract fun draw(canvas: Canvas)
+
+        public fun scale(imagePixel: Float): Float = imagePixel * overlay.scaleFactor
+
+        public fun translateX(x: Float): Float =
+            when(isImageFlipped){
+                true -> overlay.width - (scale(x) - overlay.postScaleWidthOffset)
+                false -> scale(x) - overlay.postScaleWidthOffset
+                null -> 0f
+            }
+
+
+        public fun translateY(x: Float): Float = scale(x) - overlay.postScaleHeightOffset
+
+        public fun postInvalidate() = overlay.postInvalidate()
+    }
+
+    public fun clear() {
+        synchronized(lock){
+            graphics.clear()
+        }
+        postInvalidate()
+    }
+
+    public fun add(graphic: Graphic){
+        synchronized(lock){
+            graphics.add(graphic)
+        }
+    }
+
+    public fun remove(graphic: Graphic){
+        synchronized(lock){
+            graphics.remove(graphic)
+        }
+        postInvalidate()
+    }
+
+    public fun setImageSourceInfo(imageWidth: Int, imageHeight: Int, isFlipped: Boolean){
+        Preconditions.checkState(imageWidth > 0, "image width must be positive")
+        Preconditions.checkState(imageHeight > 0, "image height must be positive")
+
+        synchronized(lock){
+            this.imageWidth = imageWidth
+            this.imageHeight = imageHeight
+            isImageFlipped = isFlipped
+            needUpdateTransformation = true
+        }
+        postInvalidate()
+    }
+
+    private fun updateTransformationIfNeeded(){
+        if(!needUpdateTransformation || imageWidth <= 0 || imageHeight <= 0)
+            return
+
+        val viewAspectRatio = (width / height).toFloat()
+        val imageAspectRatio = (imageWidth / imageHeight).toFloat()
+        postScaleHeightOffset = 0f
+        postScaleWidthOffset = 0f
+
+        if(viewAspectRatio > imageAspectRatio){
+            scaleFactor = (width / imageWidth).toFloat()
+            postScaleHeightOffset =  (width / imageAspectRatio - height) / 2
+        } else {
+            scaleFactor = (height / imageHeight).toFloat()
+            postScaleWidthOffset = (height * imageAspectRatio - width) / 2
+        }
+
+        transformationMatrix.apply {
+            reset()
+            setScale(scaleFactor, scaleFactor)
+            postTranslate(-postScaleWidthOffset, -postScaleHeightOffset)
+        }
+
+        if(isImageFlipped!!)
+            transformationMatrix.postScale(-1f, 1f, width / 2f, height / 2f)
+
+        needUpdateTransformation = false
+    }
+
+    protected override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.let{ canvas ->
+            synchronized(lock){
+                updateTransformationIfNeeded()
+                graphics.forEach { graphic ->  graphic.draw(canvas) }
+            }
+        }
+    }
+
+}
--- app/src/main/java/pl/edu/pwr/lab3/i242487/view/fragment/ObjectsFragment.kt
+++ app/src/main/java/pl/edu/pwr/lab3/i242487/view/fragment/ObjectsFragment.kt
@@ -1,23 +1,44 @@
 package pl.edu.pwr.lab3.i242487.view.fragment
 
+import android.annotation.SuppressLint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
+
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.DetectedObject
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
+
 import pl.edu.pwr.lab3.i242487.databinding.FragmentObjectsBinding
+import pl.edu.pwr.lab3.i242487.view.GraphicOverlay
+
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class ObjectsFragment : CameraPreviewFragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val mBinding get() = _binding!! as FragmentObjectsBinding
+    private val mBinding get() = _binding as FragmentObjectsBinding?
+
+    private var needUpdateGraphicOverlayImageSourceInfo = true
+
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,40 +49,148 @@ class ObjectsFragment : CameraPreviewFragment() {
         _binding = FragmentObjectsBinding.inflate(inflater, container, false)
 
         startCamera()
-        return mBinding.root
+
+        return mBinding!!.root
     }
 
     override fun startCamera() {
-//        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-//
-//        cameraProviderFuture.addListener({
-//            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-//
-//            val preview = Preview.Builder()
-//                .build()
-//                .also {
-//                    it.setSurfaceProvider(mBinding.pvCameraPreview.surfaceProvider)
-//                }
-//
-//            val imageAnalyzer = ImageAnalysis.Builder()
-//                .build()
-//                .also {
-//                    it.setAnalyzer(cameraExecutor, LabelingImageAnalyzer())
-//                }
-//
-//            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-//
-//            try{
-//                cameraProvider.unbindAll()
-//                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
-//            }catch (ex: Exception){
-//                Log.e(this.javaClass.name, "Camera binding failed", ex)
-//            }
-//        }, ContextCompat.getMainExecutor(requireContext()))
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(mBinding!!.pvCameraPreview.surfaceProvider)
+                }
+
+
+            imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, ObjectImageAnalyzer(mBinding!!.goGraphicOverlay))
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+            }catch (ex: Exception){
+                Log.e(this.javaClass.name, "Camera binding failed", ex)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private inner class ObjectImageAnalyzer(val overlay: GraphicOverlay) : ImageAnalysis.Analyzer {
+
+        @SuppressLint("UnsafeOptInUsageError")
+        override fun analyze(imageProxy: ImageProxy) {
+
+            if(needUpdateGraphicOverlayImageSourceInfo){
+                val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+
+                val isImageFlipped = lensFacing == CameraSelector.LENS_FACING_FRONT
+
+                if(rotationDegrees == 0 || rotationDegrees == 180)
+                    overlay.setImageSourceInfo(
+                        imageProxy.width, imageProxy.height, isImageFlipped
+                    )
+
+                else
+                    overlay.setImageSourceInfo(
+                        imageProxy.height, imageProxy.width, isImageFlipped
+                    )
+
+                needUpdateGraphicOverlayImageSourceInfo = false
+            }
+
+            val mediaImage = imageProxy.image
+            mediaImage?.let { _ ->
+                val image =  InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+                val options = ObjectDetectorOptions.Builder()
+                    .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+                    .enableClassification()
+                    .build()
+
+                val objectDetector = ObjectDetection.getClient(options)
+
+                objectDetector.process(image)
+                    .addOnSuccessListener {
+                        results -> results.forEach{
+                            result -> overlay.add(ObjectGraphic(overlay, result))
+                                Log.i("ObjectDetector - labels", result.labels.joinToString { it.text })
+                        }
+                    }
+                    .addOnFailureListener{
+                            ex -> Log.wtf("LAB", ex)
+                    }
+                    .addOnCompleteListener{
+                    }
+            }
+        }
+
+    }
+
+    private class ObjectGraphic(
+        overlay: GraphicOverlay,
+        private val detectedObject: DetectedObject
+    ) : GraphicOverlay.Graphic(overlay){
+
+        companion object {
+            private const val STROKE_WIDTH = 4.0f
+            private const val NUM_COLORS = 8
+
+            private val COLORS =
+                arrayOf(
+                    intArrayOf(Color.BLACK, Color.WHITE),
+                    intArrayOf(Color.WHITE, Color.MAGENTA),
+                    intArrayOf(Color.BLACK, Color.LTGRAY),
+                    intArrayOf(Color.WHITE, Color.RED),
+                    intArrayOf(Color.WHITE, Color.BLUE),
+                    intArrayOf(Color.WHITE, Color.DKGRAY),
+                    intArrayOf(Color.BLACK, Color.CYAN),
+                    intArrayOf(Color.BLACK, Color.YELLOW),
+                )
+        }
+
+        private val numColors = COLORS.size
+
+        private val boxPaints = Array(numColors){ Paint() }
+
+        init {
+            for(i in 0 until numColors){
+                boxPaints[i] = Paint().apply {
+                    color = COLORS[i][1]
+                    style = Paint.Style.STROKE
+                    strokeWidth = STROKE_WIDTH
+                }
+            }
+        }
+
+
+        override fun draw(canvas: Canvas) {
+            val colorID = if (detectedObject.trackingId == null) 0
+                else abs(detectedObject.trackingId!! % NUM_COLORS)
+
+            val rect = RectF(detectedObject.boundingBox)
+            val x0 = translateX(rect.left)
+            val x1 = translateX(rect.right)
+            rect.left = min(x0, x1)
+            rect.right = max(x0, x1)
+            rect.top = translateY(rect.top)
+            rect.bottom = translateY(rect.bottom)
+
+            canvas.drawRect(rect, boxPaints[colorID])
+        }
+
+    }
+
 }
